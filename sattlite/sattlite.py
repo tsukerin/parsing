4d536a5c8768dfdb8217c0858ab8932b08de6993
tsukerin <107303964+tsukerin@users.noreply.github.com>
2024-04-06 00:45:41 +0300

    change

--- sattlite/sattlite.py
+++ sattlite/sattlite.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import csv
-from tqdm import tqdm
+import sqlite3
 
 def parse_page(url):
     response = requests.get(url)
@@ -15,6 +15,7 @@ def parse_page(url):
             full_text = a.find('span', class_='description', id=f'ContentPlaceHolder1_rptrNews_lblNewsDescription_{i}').text.strip()
             link = a.find('a')['href']
             data.append([date, title, full_text, link])
+            cursor.execute('INSERT INTO sattlite (date, title, full_text, link) VALUES (?, ?, ?, ?)', (date, title, full_text, link))
         return data
     return None
 
@@ -25,13 +26,34 @@ def save_csv(data, filename):
         wr.writerow(['Date', 'Title', 'Full Text', 'Link'])
         wr.writerows(data)
 
+def main():
+    base_url = 'https://www.everythingrf.com/news?page='
+    all_data = []
+    page = requests.get(base_url)
+    for page_num in range (1,11):
+        url = base_url + str(page_num) + '&tags=satellite'
+        page_data = parse_page(url)
+        if page_data:
+            all_data.extend(page_data)
+    save_csv(all_data, 'satellite_news.csv')
 
-base_url = 'https://www.everythingrf.com/news?page='
-all_data = []
-page = requests.get(base_url)
-for page_num in tqdm(range(1,11)):
-    url = base_url + str(page_num) + '&tags=satellite'
-    page_data = parse_page(url)
-    if page_data:
-        all_data.extend(page_data)
-save_csv(all_data, 'satellite_news.csv')
+db = sqlite3.connect('satellite.db')
+cursor = db.cursor()
+
+cursor.execute(
+        '''
+        CREATE TABLE IF NOT EXISTS sattlite (
+        id INTEGER PRIMARY KEY, 
+        date TEXT,
+        title TEXT,
+        full_text TEXT,
+        link TEXT
+        )
+        '''
+    )
+
+main()
+
+
+db.commit()
+db.close()
